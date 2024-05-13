2930434c0abb377c98b19a2f35c0f0b06c389793
mominjongofurjonov <mominjongofurjonov2007@gmail.com>
2024-05-14 00:53:43 +0500

    first commit

---
+++ src/routes/notFound/NotFound.tsx
@@ -0,0 +1,8 @@
+
+const NotFound = () => {
+  return (
+    <img width={1248} height={600} style={{margin: 'auto', display: 'block'}} src="https://hyundaikinhduongvuong.com/images/404.jpg" alt="" />
+  )
+}
+
+export default NotFound
